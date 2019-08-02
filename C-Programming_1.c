76e3f98dde64eee52b693d15419c5978978eaf05
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-08-02 18:33:15 +0300

    Changed file name. Added some basic C information

---
+++ C-Programming_1.c
@@ -0,0 +1,23 @@
+//C Programming Begins
+
+/* #include içe aktarır => <kütüphane.adi>
+   #define variable yerine en üst kısımda kullanılabilir.
+   
+   ';' bu dilde vazgeçilmez.
+   
+   \n : bir satır alta geçer.
+   \t : bir tab boşluk atar.
+
+   int : tam sayı  '%d' 
+   float : ondalıklı sayı '%f'
+   double : daha derin kesirli sayılar '%1f'
+   char : harfler (tırnak içi yazılır) '%s'
+
+   printf : Ekrana yazdırır.
+   = printf('Enter a number');
+   scanf : Kullanıcıdan giriş bekler. Girilen değeri almak için değişken adı başına & eklenir.
+   = scanf('%d',&number);
+
+   printf(sizeof(degiskenadi)) : değişkenin kapladığı alanı verir.
+
+    */
