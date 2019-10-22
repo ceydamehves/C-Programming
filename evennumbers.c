81a4df964f9489765e39908643d7a13cae7277b2
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-10-22 22:20:53 +0300

    This code show the even numbers between 0-100

---
+++ evennumbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+//This code show the even numbers between 0-100
+int main(){
+    int x = 0;
+    while (x<=100){
+    	if(x%2==0){
+    	printf("%d\n",x);
+		}
+		x++;
+	}
+	
+}
